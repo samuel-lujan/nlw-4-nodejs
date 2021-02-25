5b9cc001fa780dcb0029e8192ab151c6cd01fdca
Samuel Lujan <samuel_lujan@hotmail.com>
2021-02-24 23:09:34 -0300

    Adicionando controller e criando rota para cadastrar usuario

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,11 @@
 import 'reflect-metadata';
 import "./database"
 import express from 'express';
+import { router } from './routes';
 
 const app = express();
 
-app.get('/users', (request, response) => {
-    return response.json({message: "Hello World"});
-});
+app.use(express.json);
+app.use(router);
 
 app.listen(3333, () => console.log("server is running"));
