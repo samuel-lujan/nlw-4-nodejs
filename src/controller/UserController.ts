24c95d6d4ffa74e82eba09888cb3840e71e9e2b7
Samuel Lujan <samuel_lujan@hotmail.com>
2021-02-26 07:03:51 -0300

    Finalizando cadastro de usuario

--- src/controller/UserController.ts
+++ src/controller/UserController.ts
@@ -1,10 +1,28 @@
 import {Request, Response} from "express";
+import { getRepository } from "typeorm";
+import { User } from "../models/User";
 
 class UserController {
     async create(request: Request, response: Response){
-        const body =  request.body;
-        console.log(body);
-        return response.send();
+        const {name, email} =  request.body;
+
+        const userRepository = getRepository(User);
+
+        const userAlreadyExists = await userRepository.findOne({
+            email
+        });
+
+        if(userAlreadyExists){
+            return response.status(400).json({
+                error: "This user already Exists"
+            });
+        }
+
+        const user = userRepository.create({ name, email});
+
+        await userRepository.save(user);
+
+        return response.json(user);
     }
 }
 
